1de1a6c13d2199c3605171756b431fcbf88add40
Decode8079 <61305527+Decode8079@users.noreply.github.com>
2020-11-21 16:57:10 -0400

    Initial commit

---
+++ javascript.js
@@ -0,0 +1,10 @@
+
+
+ function myFunction() {
+   var x = document.getElementById("nav-div");
+   if (x.style.display === "none" || "flex") {
+     x.style.display = "block";
+   } else {
+     x.style.display = "none";
+   }
+ }
